ac075a8ecd6b855e60815a060d34060da1db6d00
Avinash <avinash.pathak@grapecity.com>
2022-10-28 22:48:31 +0530

    added stack

---
+++ stack/stack.go
@@ -0,0 +1,28 @@
+package stack
+
+import "fmt"
+
+type Stack[T any] struct {
+	stack []T
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.stack = append(s.stack, value)
+}
+
+func (s *Stack[T]) Pop() {
+	if len(s.stack) <= 1 {
+		s.stack = []T{}
+		return
+	}
+
+	s.stack = s.stack[0 : len(s.stack)-1]
+}
+
+func (s *Stack[T]) Peek() T {
+	return s.stack[len(s.stack)-1]
+}
+
+func (s *Stack[T]) Print() {
+	fmt.Println(s.stack)
+}
