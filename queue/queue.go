e6f23a8e96b9df9765ce24ad824b29115534c873
Avinash <avinash.pathak@grapecity.com>
2022-10-30 18:14:10 +0530

    implemented Queue using array

---
+++ queue/queue.go
@@ -0,0 +1,40 @@
+package queue
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Queue[T comparable] struct {
+	queue []T
+}
+
+func (q *Queue[T]) EnQueue(value T) {
+	q.queue = append(q.queue, value)
+}
+
+func (q *Queue[T]) Print() {
+	fmt.Println(q.queue)
+}
+
+func (q *Queue[T]) DeQueue() T {
+	if len(q.queue) == 0 {
+		fmt.Println("queue is empty")
+		errors.New("empty name")
+	}
+	var res T = q.queue[0]
+	q.queue = q.queue[1:]
+
+	return res
+
+}
+
+func (q *Queue[T]) Peek() T {
+	if len(q.queue) == 0 {
+		fmt.Println("queue is empty")
+		errors.New("empty name")
+	}
+
+	return q.queue[len(q.queue)-1]
+
+}
