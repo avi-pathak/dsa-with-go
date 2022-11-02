33c295c13e145c9bcb96cf7ce2b260c5976caad1
Avinash <avinash.pathak@grapecity.com>
2022-11-02 22:47:32 +0530

    minor changes

--- stack/stack.go
+++ stack/stack.go
@@ -10,13 +10,13 @@ func (s *Stack[T]) Push(value T) {
 	s.stack = append(s.stack, value)
 }
 
-func (s *Stack[T]) Pop() {
-	if len(s.stack) <= 1 {
-		s.stack = []T{}
-		return
+func (s *Stack[T]) Pop() T {
+	if len(s.stack) >= 0 {
+		var value T = s.stack[len(s.stack)-1]
+		s.stack = s.stack[0 : len(s.stack)-1]
+		return value
 	}
 
-	s.stack = s.stack[0 : len(s.stack)-1]
 }
 
 func (s *Stack[T]) Peek() T {
